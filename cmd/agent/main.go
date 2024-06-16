6506415666d4d4167b851410dd91753187731941
root-gabriel <dikof.f@yandex.ru>
2024-06-16 15:33:03 +0300

    iter2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,10 +3,25 @@ package main
 import (
     "log"
     "net/http"
+    "time"
     "github.com/root-gabriel/metrics/internal/handlers"
 )
 
+func sendTestMetrics() {
+    client := &http.Client{}
+    for {
+        req, _ := http.NewRequest("POST", "http://localhost:8080/update/counter/testCounter/1", nil)
+        _, err := client.Do(req)
+        if err != nil {
+            log.Println("Error sending metric:", err)
+        }
+        time.Sleep(1 * time.Second)
+    }
+}
+
 func main() {
+    go sendTestMetrics() // Запускаем генерацию метрик в отдельной горутине
+
     http.HandleFunc("/update/counter/", handlers.UpdateCounter)
     http.HandleFunc("/update/gauge/", handlers.UpdateGauge)
     http.HandleFunc("/value/counter/", handlers.GetCounterValue)
